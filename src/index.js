09299b9543f2c57ec80cf531e410a3416e882ea4
andreduarte99 <andre.duarte16@outlook.com>
2025-01-12 21:05:11 -0300

    terminando aula 2

--- src/index.js
+++ src/index.js
@@ -1,49 +1,12 @@
 import { Personagem } from "./modules/personagem.js";
-/*
-const personagemPedrinho = {
-  nome: "Pedrinho",
-  vida: 7,
-  mana: 12,
-  level: 5,
-  tipo: "Mago",
-};
-*/
-/*
-const personagemJose = {
-  nome: "Jose",
-  vida: 7,
-  mana: 6,
-  level: 3,
-  tipo: "Arqueiro",
-};
-*/
+import { PersonagemView } from "./components/personagem-view.js";
+import { Mago } from "./modules/personagem.js";
 
-const personagemAna = {
-  nome: "Ana",
-  vida: 8,
-  mana: 10,
-};
+const personagemPedrinho = new Personagem("Pedrinho", 5, "Mago");
+const personagemJose = new Personagem("Jose", 3, "Arqueiro");
 
-const personagemPedrinho = new Personagem();
+const personagens = [personagemPedrinho, personagemJose];
 
-personagemPedrinho.nome = "Pedrinho";
-personagemPedrinho.mana = 12;
-personagemPedrinho.vida = 7;
-personagemPedrinho.tipo = "Mago";
-personagemPedrinho.level = 5;
+new PersonagemView(personagens).render();
 
-const personagemJose = new Personagem();
-personagemJose.nome = "Jose";
-personagemJose.tipo = "Arqueiro";
-personagemJose.level = 3;
-
-console.log(
-  "Insignia de " +
-    personagemPedrinho.nome +
-    ": " +
-    personagemPedrinho.obterInsignia()
-);
-
-console.log(
-  "Insignia de " + personagemJose.nome + ": " + personagemJose.obterInsignia()
-);
+//console.log(new Mago("gelo", 7, 9));
--- src/modules/personagem.js
+++ src/modules/personagem.js
@@ -1,15 +1,35 @@
 export class Personagem {
   nome;
-  vida;
-  mana;
+  vida = 100;
+  mana = 100;
   level;
   tipo;
   descricao;
+
+  constructor(nome, level, tipo) {
+    this.nome = nome;
+    this.level = level;
+    this.tipo = tipo;
+    //this.vida = 100;
+    //this.mana = 100;
+  }
 
   obterInsignia() {
     if (this.level >= 5) {
       return `Implacavel ${this.tipo}`;
     }
     return `${this.tipo} iniciante`;
+  }
+}
+
+export class Mago {
+  elementoMagico;
+  levelMagico;
+  inteligencia;
+
+  constructor(elementoMagico, levelMagico, inteligencia) {
+    this.elementoMagico = elementoMagico;
+    this.levelMagico = levelMagico;
+    this.inteligencia = inteligencia;
   }
 }
